88df41174fba6096a9c7f4b575bcbe94389db8a4
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-06-10 23:46:38 +0900

    흐으

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,14 @@ const fs = require('fs');
 var router = express.Router();
 
 router.put('/:pid', (req, res, next) => {
+  console.log(req.body);
   fs.writeFile("./public/json/" + req.params.pid + ".json", req.body.data, err => {
-    if (err) res.status(400);
-    else res.status(200);
+
+    console.log(req.body.data);
+    if (err) {
+      console.log(err);
+      //res.status(400);
+    } else res.status(200);
   });
   res.end();
 });
@@ -15,7 +20,6 @@ router.put('/:pid', (req, res, next) => {
 router.get('/:pid', (req, res, next) => {
   fs.readFile("./public/json/" + req.params.pid + ".json", "utf-8", (err, data) => {
     if (err) {
-      console.log(err);
       res.end("{}");
       return;
     }
